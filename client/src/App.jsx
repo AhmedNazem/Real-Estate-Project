4a4af9cb6cb8413cb58a1f5c52f7f9fc17de289c
AhmedNazem <aahhaammdd55@gmail.com>
2023-11-24 18:57:18 +0300

    create database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/about";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 function App() {
-  return <div>app</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
